fa1bbe12c88d7b04b85288a690f58be7b91064a0
Farook13 <umarulfarook787@gmail.com>
2025-02-17 20:46:12 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ from info import AUTH_CHANNEL, LONG_IMDB_DESCRIPTION, MAX_LIST_ELM, SHORTLINK_UR
 from imdb import Cinemagoer 
 import asyncio
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
-from pyrogram.errors import FloodWait, UserIsBlocked, MessageNotModified, PeerIdInvalid
+from pyrogram.errors import FloodWait, UserIsBlocked, MessageNotModified
 from pyrogram import enums
 from typing import Union
 from Script import script
